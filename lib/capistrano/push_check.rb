ae86de1c62044a82033629e0f1885b9e4fdd84ce
labocho <labocho@penguinlab.jp>
2021-06-15 15:55:40 +0900

    bundle gem capistrano-push_check

---
+++ lib/capistrano/push_check.rb
@@ -0,0 +1,8 @@
+require "capistrano/push_check/version"
+
+module Capistrano
+  module PushCheck
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/capistrano/push_check_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Capistrano::PushCheck do
+  it "has a version number" do
+    expect(Capistrano::PushCheck::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
